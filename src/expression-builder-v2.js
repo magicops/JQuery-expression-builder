557d76ddcc5a40e784a846a05944cdd57cd77832
Artur Drobinskiy <artur.drobinskiy@loewensteinmedical.de>
2020-07-02 15:38:24 +0700

    make project buildable for everyone

--- src/expression-builder-v2.js
+++ src/expression-builder-v2.js
@@ -5,7 +5,7 @@ var __extends = (this && this.__extends) || (function () {
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
             function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
         return extendStatics(d, b);
-    }
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -794,7 +794,6 @@ jQuery.fn.extend({
                     setCursorPosition(i + 2);
                     break;
                 }
-            expressionInput.trigger('change');
             validation();
         }
         return {
